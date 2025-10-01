d8934f040b0c0efdd2d241cdffa6f7b38f1ec073
dammybundlez <hunchosage@gmail.com>
2025-10-01 16:15:34 -0700

    push

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./business.html', './job.html' , './event.html' ],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ["Gilroy", "sans-serif"], 
+        arial: ["Arial", "sans-serif"],
+      },
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
